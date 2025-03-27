bcada6c163f774b83f6548ae722562b77c9dc9bf
doug <doug@4all.digita>
2025-03-27 13:34:33 -0700

    Add new block

---
+++ inc/page-modules/feature-tiles.php
@@ -0,0 +1,56 @@
+<?php
+    $title = get_sub_field( 'title' );
+    $text = get_sub_field( 'text' );
+    $backgroundImage = get_sub_field( 'background_image' );
+?>
+<section class="feature-tiles">
+  <div class="container">
+    <div class="heading-text">
+      <h1 class="title">
+        The Future of Water <span>is Now</span>
+      </h1>
+    </div>
+
+    <div class="graphic-and-description">
+      <div class="water-graphic">
+        <!-- Placeholder for the dotted globe graphic -->
+        <div class="circle-dots"></div>
+      </div>
+      <div class="description">
+        <p>
+          Aquaria is revolutionizing how we access water with atmospheric water generation. Our atmospheric water generators produce water where you need it, when you need it.
+        </p>
+      </div>
+    </div>
+
+    <div class="features">
+      <div class="feature-box">
+        <div class="icon">
+          <!-- Shield icon placeholder -->
+          <span class="icon-shield"></span>
+        </div>
+        <div class="feature-text">
+          <h3>Premium water for<br><span>Any use case</span></h3>
+        </div>
+      </div>
+
+      <div class="feature-box">
+        <div class="icon">
+          <span class="icon-shield"></span>
+        </div>
+        <div class="feature-text">
+          <h3>Protection against<br><span>drought and contamination</span></h3>
+        </div>
+      </div>
+
+      <div class="feature-box">
+        <div class="icon">
+          <span class="icon-shield"></span>
+        </div>
+        <div class="feature-text">
+          <h3>Scalable to meet<br><span>any water need, anywhere</span></h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
